f734b637dfc5a35d4fe5c1bc856d943bf22dc7d2
Sébastien Weber <seba.weber@gmail.com>
2020-11-09 16:12:32 +0100

    Initial commit

---
+++ plugin_info.py
@@ -0,0 +1,11 @@
+## To modify by developper(s) of the plugin
+SHORT_PLUGIN_NAME = 'picoquant'  #for instance daqmx
+package_url = 'https://github.com/CEMES-CNRS/pymodaq_plugins_picoquant' #to modify
+description = 'Set of PyMoDAQ plugins for Picoquant instruments (TimeHarp260, ...)'
+
+author = 'Sébastien Weber'
+author_email = 'sebastien.weber@cemes.fr'
+
+#packages required for your plugin:
+packages_required = []
+##
